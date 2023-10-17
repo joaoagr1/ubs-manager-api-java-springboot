8c1c5aa10f82783f0f7ad1e7c28047f4d6312c80
joaoagr1 <joao.rollo1@gmail.com>
2023-10-16 23:14:50 -0400

    16.10.23:20

--- api-medicos-java/src/main/java/med/voll/api/controller/MedicoController.java
+++ api-medicos-java/src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("medicos")
@@ -47,31 +50,47 @@ public class MedicoController {
 
 
     @CrossOrigin
-    @GetMapping ("/{id}")
-    public String buscaPorId(@PathVariable Long id) {
-        return "oi";
-
+    @GetMapping("/{id}")
+    public ResponseEntity<String> buscaPorId(@PathVariable Long id) {
+        try {
+            Medico medico = repository.getReferenceById(id);
+            if (medico != null) {
+                return ResponseEntity.ok(medico.toString());
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception ex) {
+            // Tratar exceção apropriadamente
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno do servidor");
+        }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
